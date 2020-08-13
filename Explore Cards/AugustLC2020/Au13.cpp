177cf516b998c082e4e9916c03e856b94fa5166d
Mayank Talwar <dcmtalwar@gmail.com>
2020-08-13 18:37:13 +0530

    v0.1

---
+++ Explore Cards/AugustLC2020/Au13.cpp
@@ -0,0 +1,38 @@
+//Version 1: Made a Permutation Iterator x.x
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+class CombinationIterator
+{
+public:
+  CombinationIterator(string characters, int combinationLength)
+  {
+    this->s = characters;
+    this->len = combinationLength;
+  }
+  string next()
+  {
+    next_permutation(s.begin(), s.end());
+    return this->s;
+  }
+  bool hasNext()
+  {
+    string x = this->s;
+    return next_permutation(x.begin(), x.end());
+  }
+private:
+  string s;
+  int len;
+};
+int main()
+{
+  int l = 0;
+  string s = "";
+  cin >> l >> s;
+  CombinationIterator *it = new CombinationIterator(s, l);
+  while(it->hasNext())
+  {
+    cout << it->next() << "\n";
+  }
+}
