9787af8063e6de171c9d9b32b587c82869011591
Mayank Talwar <dcmtalwar@gmail.com>
2020-08-14 10:24:47 +0530

    v1

--- Explore Cards/AugustLC2020/Au13.cpp
+++ Explore Cards/AugustLC2020/Au13.cpp
@@ -1,5 +1,7 @@
 //Version 0.1: Made a Permutation Iterator x.x
+//Version 1.0: Problem solved, pretty satisfied with solution
 #include <iostream>
+#include <vector>
 #include <string>
 #include <algorithm>
 using namespace std;
@@ -13,17 +15,58 @@ public:
   }
   string next()
   {
-    next_permutation(s.begin(), s.end());
-    return this->s;
+    updateCombination();
+    string x = "";
+    for(int i = 0; i < len; i++)
+    {
+      x.push_back(s[currentCombination[i]]);
+    }
+    return x;
   }
   bool hasNext()
   {
-    string x = this->s;
-    return next_permutation(x.begin(), x.end());
+    for(int i = 0; i < len; i++)
+    {
+      if(currentCombination.size() == 0 ||
+        s.length() - len + i != currentCombination[i])
+      {
+        return 1;
+      }
+    }
+    return 0;
   }
 private:
   string s;
   int len;
+  vector<int> currentCombination;
+  void updateCombination()
+  {
+    if(currentCombination.size() == 0)
+    {
+      currentCombination.resize(len);
+      for(int i = 0; i < len; i++)
+      {
+        currentCombination[i] = i;
+      }
+    }
+    else
+    {
+      int last = currentCombination.size() - 1;
+      for(int i = 0; i > -last - 1; i--)
+      {
+        if(currentCombination[last + i] != s.size() - 1 + i)
+        {
+          currentCombination[last + i]++;
+          while(i != 0)
+          {
+            currentCombination[last + i + 1] = currentCombination[last + i] + 1;
+            i++;
+          }
+          break;
+        }
+      }
+    }
+  }
 };
 int main()
 {
@@ -36,3 +79,7 @@ int main()
     cout << it->next() << "\n";
   }
 }
+/*
+[null,true,"fik",true,"fil",true,"fin","fiu","fiy",true,true,"fkl",true,true,"fkn",true,"fku",true,true,true,"fky","fln",true,"flu",true,"fly","fnu",true,true,"fny","fuy",true,true,"iky",true,"iln","ilu","ily","inu",true,true,"iny","iuy",true,true,"kly","knu",true,"kny",true,true,true,"kuy","lny",true,true,true,true,"luy",true,"nuy",false,"nuy","nuy","nuy","nuy","nuy","nuy",false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+[null,true,"fik",true,"fil",true,"fin","fiu","fiy",true,true,"fkl",true,true,"fkn",true,"fku",true,true,true,"fky","fln",true,"flu",true,"fly","fnu",true,true,"fny","fuy",true,true,"ikl",true,"ikn","iku","iky","iln",true,true,"ilu","ily",true,true,"inu","iny",true,"iuy",true,true,true,"kln","klu",true,true,true,true,"kly",true,"knu",true,"kny","kuy","lnu","lny","luy","nuy",false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+*/
