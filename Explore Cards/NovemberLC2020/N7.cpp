35bd7b62e2c023f008c9fdccdcdf934ea9b9ec67
Mayank Talwar <dreadarceus@gmail.com>
2021-02-19 17:01:32 +0530

    feat: housekeeping

--- Explore Cards/NovemberLC2020/N7.cpp
+++ Explore Cards/NovemberLC2020/N7.cpp
@@ -1,6 +1,5 @@
 //v1: Reverse the lists, do add with carry: Accepted
-//Not bad at all BUT the question says try to do it without modifying the input lists
-//So naturally, thats coming up...
+//v2: Memory abuse?
 #include <iostream>
 using namespace std;
 
@@ -37,52 +36,11 @@ void printList(ListNode* head)
     cout << "\n";
 }
 
-ListNode* reverseList(ListNode* h)
-{
-    if(h == NULL)
-    {
-        return h;
-    }
-    ListNode* after = h->next;
-    h->next = NULL;
-    ListNode* after2 = NULL;
-    while(after)
-    {
-        after2 = after->next;
-        after->next = h;
-        h = after;
-        after = after2;
-    }
-    return h;
-}
-
 ListNode* addLists(ListNode* h1, ListNode* h2)
 {
-    h1 = reverseList(h1);
-    h2 = reverseList(h2);
     ListNode* ansH = new ListNode;
     ListNode* ansT = ansH;
-    int carry = 0;
-    while(h1 || h2)
-    {
-        int sum = ((h1) ? h1->val : 0) + ((h2) ? h2->val : 0);
-        cout << sum << "\n";
-        ListNode* ansNext = new ListNode;
-        ansNext->val = (sum % 10 + carry) % 10;
-        ansT->next = ansNext;
-        ansT = ansNext;
-        if(h1)
-            h1 = h1->next;
-        if(h2)
-            h2 = h2->next;
-        carry = (sum + carry) / 10;
-    }
-    if(carry != 0)
-    {
-        ansT->next = new ListNode;
-        ansT->next->val = carry;
-    }
-    ansH = reverseList(ansH->next);
+
     return ansH;
 }
 
